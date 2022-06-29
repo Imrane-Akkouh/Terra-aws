bec8fb3c272c699dde808954ec918f8a2844507f
Imrane Akkouh <akkouh.imrane@gmail.com>
2022-06-29 19:04:05 +0100

    created a cluster of EC2 instances with auto-scaling and load balancing

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-2"
 }
-
+/*
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0629230e074c580f2"
   instance_type = "t2.micro"
@@ -9,7 +9,7 @@ resource "aws_instance" "my_ec2" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "hello, from Terraform" > index.html
+              echo "hello, from Terraform" > index.html                               //THIS IS ONE APPLICATION SERVER SETUP
               nohup busybox httpd -f -p 8080 &
               EOF
 
@@ -17,8 +17,15 @@ resource "aws_instance" "my_ec2" {
     name = "Terraform-Training"
   }
 }
+*/
 
-resource "aws_security_group" "security_group" {
+variable "server_port" {
+description = "The port the server will use for HTTP requests"
+type = number
+default = 8080
+}
+
+resource "aws_security_group" "example" {
 
   name = "terraform security group"
 
@@ -29,4 +36,117 @@ resource "aws_security_group" "security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.example.id]
+  user_data       = <<-EOF
+                    #!/bin/bash
+                    echo "Hello, World" > index.html
+                    nohup busybox httpd -f -p ${var.server_port} &
+                    EOF
+  # Required when using a launch configuration with an auto scaling group.
+  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_vpc" "default" {
+default = true
+}
+
+data "aws_subnet_ids" "default" {
+vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+  target_group_arns    = [aws_lb_target_group.asg.arn]
+  health_check_type    = "ELB"
+  min_size             = 2
+  max_size             = 10
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
